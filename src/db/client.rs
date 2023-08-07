b469715fa0822674a55b8d303dbbccefc32840c5
deejiw <deejiw@gmail.com>
2023-08-07 19:14:02 +0700

    fix: record

--- src/db/client.rs
+++ src/db/client.rs
@@ -1,8 +1,5 @@
 use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
-pub struct PgPoolWrapper{
-  pub pool: PgPool,
-}
 
 pub async fn establish_connection() -> PgPool {  
   let db_url = std::env::var("DATABASE_URL").expect("Unable to read DATABASE_URL env var");
--- src/db/record.rs
+++ src/db/record.rs
@@ -1,54 +1,46 @@
-use tauri::command;
-use tauri::State;
+use sqlx::types::{JsonValue};
+use sqlx::PgPool;
 use serde::{Deserialize, Serialize};
-// use sqlx::types::time::PrimitiveDateTime;
-use super::client;
-
+use bigdecimal::BigDecimal;
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Record {
-  pub id: Option<i64>,
-  pub project_name: Option<String>,
-  pub service_name: Option<String>,
-  pub api_name: Option<String>,
-  pub image_tag: Option<String>,
+  pub release: Option<String>,
+  pub execution_type: Option<String>,
+  pub project: Option<String>,
+  pub service: Option<String>,
+  pub flow: Option<String>,
+  pub tag: Option<String>,
   pub vu: Option<i16>,
   pub duration: Option<String>,
-  pub tps: Option<f64>,
-  pub error_rate: Option<f64>,
-  pub rt_avg: Option<f64>,
-  pub rt_min: Option<f64>,
-  pub rt_max: Option<f64>,
-  pub rt_p90: Option<f64>,
-  pub rt_p95: Option<f64>,
-  pub rt_p99: Option<f64>,
+  pub tps: Option<BigDecimal>,
+  pub error_rate: Option<BigDecimal>,
+  pub rt_avg: Option<BigDecimal>,
+  pub rt_min: Option<BigDecimal>,
+  pub rt_max: Option<BigDecimal>,
+  pub rt_p90: Option<BigDecimal>,
+  pub rt_p95: Option<BigDecimal>,
+  pub rt_p99: Option<BigDecimal>,
   pub is_cpu_below_request: Option<bool>,
-  pub is_pipeline: Option<bool>,
+  pub resource_map: Option<JsonValue>,
+  pub replica_map: Option<JsonValue>,
+  pub cpu_utilization: Option<Vec<BigDecimal>>,
+  pub cpu_request: Option<Vec<BigDecimal>>,
+  pub cpu_limit: Option<Vec<BigDecimal>>,
+  pub memory_utilization: Option<Vec<BigDecimal>>,
+  pub memory_request: Option<Vec<BigDecimal>>,
+  pub memory_limit: Option<Vec<BigDecimal>>,
   pub timestamp: Option<String>,
 }
 
-#[command]
-pub async fn list_record(state: State<'_, client::PgPoolWrapper>) -> Result<Vec<Record>, String> {
+
+pub async fn list_record(c: PgPool) -> Result<Vec<Record>, String> {
   let rows: Vec<Record> = sqlx::query_as!(
     Record,
-    r#"select * from public.records"#
-  ).fetch_all(&state.pool)
+    r#"select * from record.report"#
+  ).fetch_all(&c)
     .await.expect("Unable to list Record");
 
   // println!("{:?}", rows);
 
   Ok(rows)
-}
-
-#[command]
-pub async fn get_record(state: State<'_, client::PgPoolWrapper>, id: i64) -> Result<Record, String> {
-  let row: Record = sqlx::query_as!(
-    Record,
-    r#"select * from public.records where id = ($1)"#,
-    id
-  ).fetch_one(&state.pool)
-    .await.expect("Unable to get Record");
-
-  // println!("{:?}", row);
-
-  Ok(row)
 }
