a504c520223466157a9f25fcf1f4cb51c160a9b0
deejiw <deejiw@gmail.com>
2023-08-08 12:08:58 +0700

    feat: rename healthcheck path

--- src/route/router.rs
+++ src/route/router.rs
@@ -6,7 +6,7 @@ use crate::{handler::handler, state::state::AppState};
 
 pub fn create_router(app_state: Arc<AppState>) -> Router {
     Router::new()
-        .route("/api/healthz", get(handler::health_checker_handler))
+        .route("/healthz", get(handler::health_checker_handler))
         .route(
             "/api/project/:project/release/:release",
             get(handler::get_report_handler),
